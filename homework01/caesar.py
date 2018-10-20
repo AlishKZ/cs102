1b8477d31cd98c584ead9c38da1ee33c46e99606
AlishKZ <a.alisher.kz2@gmail.com>
2018-10-20 22:34:42 +0300

    Добавлен шаблон для шифра Цезаря

---
+++ homework01/caesar.py
@@ -0,0 +1,44 @@
+def encrypt_caesar(plaintext):
+    """
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+    ciphertext = ''
+    for char in plaintext:
+        if (char <= 'z' and char >= 'a') or (char <= 'Z' and char >= 'A'):
+            code = ord(char) + 3
+            if code > ord('Z') or code > ord('z'):
+                code -= 26
+            ciphertext += chr(code)
+        else:
+            ciphertext += char
+    return ciphertext
+
+
+def decrypt_caesar(ciphertext):
+    """
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
+    plaintext = ''
+    for char in ciphertext:
+        if (char <= 'z' and char >= 'a') or (char <= 'Z' and char >= 'A'):
+            code = ord(char) - 3
+            if code < ord('a') and code > ord('Z') or code < ord('A'):
+                code += 26
+            plaintext += chr(code)
+        else:
+            plaintext += char
+return plaintext
